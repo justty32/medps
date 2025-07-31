5e0b634641d7333fe9f268441eff72d836d5a30b
GuanYu Lu <guanyu.lu@teco.com.tw>
2025-07-31 14:23:34 +0800

    expand bin_fwr.hpp;

--- src/gcore/util/bin_fwr.hpp
+++ src/gcore/util/bin_fwr.hpp
@@ -1,11 +1,15 @@
 #pragma once
 #include <sstream>
 #include <vector>
+#include <map>
+#include <unordered_map>
+#include <array>
 
 // binary stream write/read
 namespace BinFSR {
 	using istream_t = std::istringstream;
 	using ostream_t = std::ostringstream;
+
 	template<typename T>
 	inline void read(T* t, istream_t& fs) {
 		fs.read((char*)t, sizeof(T));
@@ -14,6 +18,7 @@ namespace BinFSR {
 	inline void write(T* t, ostream_t& fs) {
 		fs.write((char*)t, sizeof(T));
 	}
+
 	template<typename T>
 	void read(std::vector<T>* vec, istream_t& fs) {
 		size_t size = 0;
@@ -29,4 +34,62 @@ namespace BinFSR {
 		for (int i = 0; i < size; i++)
 			write(&(vec->at(i)), fs);
 	}
+
+	template<typename T, size_t S>
+	void read(std::array<T, S>* arr, istream_t& fs) {
+		size_t size = 0;
+		read(&size, fs);
+		for (int i = 0; i < size && i < S; i++)
+			read(&(arr->at(i)), fs);
+	}
+	template<typename T, size_t S>
+	void write(std::array<T, S>* arr, ostream_t& fs) {
+		size_t size = S;
+		write(&size, fs);
+		for (int i = 0; i < size; i++)
+			write(&(arr->at(i)), fs);
+	}
+
+	template<typename Tk, typename Tv>
+	void read(std::map<Tk, Tv>* m, istream_t& fs) {
+		size_t size = 0;
+		read(&size, fs);
+		for (int i = 0; i < size; i++) {
+			Tk k; Tv v;
+			read(&k, fs);
+			read(&v, fs);
+			(*m)[k] = v;
+		}
+	}
+	template<typename Tk, typename Tv>
+	void write(std::map<Tk, Tv>* m, ostream_t& fs) {
+		size_t size = m->size();
+		write(&size, fs);
+		for(auto& [k,v]: *m){
+			write(&k, fs);
+			write(&v, fs);
+		}
+	}
+
+	template<typename Tk, typename Tv>
+	void read(std::unordered_map<Tk, Tv>* m, istream_t& fs) {
+		size_t size = 0;
+		read(&size, fs);
+		for (int i = 0; i < size; i++) {
+			Tk k; Tv v;
+			read(&k, fs);
+			read(&v, fs);
+			(*m)[k] = v;
+		}
+	}
+	template<typename Tk, typename Tv>
+	void write(std::unordered_map<Tk, Tv>* m, ostream_t& fs) {
+		size_t size = m->size();
+		write(&size, fs);
+		for(auto& [k,v]: *m){
+			write(&k, fs);
+			write(&v, fs);
+		}
+	}
+
 }
--- src/gcore/util/observe.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "scene.hpp"
-
-class Observer : public Scene {
-	OBJ_INIT_DEF(21, Observer, Scene)
-public:
-};
